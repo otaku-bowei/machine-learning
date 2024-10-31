f65c020598af868ea30901d275e0abafd0497481
王柏伟 <wangbw3@chinatelecom.cn>
2024-10-31 17:42:34 +0800

    #comment liner-regression-test

--- homework/liner_regression/liner_regression_student.py
+++ homework/liner_regression/liner_regression_student.py
@@ -2,6 +2,8 @@
 import numpy as np
 import pandas as pd
 import tool.matplotlib.draw_matplotlib as tm
+import sklearn as sk
+import sklearn.linear_model as lm
 
 
 # read_data 读取数据
@@ -10,8 +12,8 @@ def read_data() -> pd.DataFrame:
     print(df.head())
     print(df.describe())
     df = df.loc[:, ['reading score', 'writing score']]
-    df_np = df.to_numpy()
-    tm.draw_2d_point(df_np.T[0], df_np.T[1])
+    # df_np = df.to_numpy()
+    # tm.draw_2d_point(df_np.T[0], df_np.T[1])
     return df
 
 
@@ -24,22 +26,40 @@ def compute_cost(X, y, w, b):
     return cost
 
 
-# train 训练，整理数据，利用梯度下降，根据损失函数找到凸函数的最点
-def train():
+# pd_data_to_np pd数据转np
+def pd_data_to_np():
     data = read_data()
     cols = data.shape[1]
     X = data.iloc[:, 0:cols - 1]  # X是所有行，去掉最后一列
     y = data.iloc[:, cols - 1:cols]  # y是所有行，最后一列
     x_np = X.to_numpy()
     y_np = y.to_numpy()
+    return x_np, y_np
+
+
+# train 训练，整理数据，利用梯度下降，根据损失函数找到凸函数的最点
+def train():
+    x_np, y_np = pd_data_to_np()
     # 此处可以对比，当一开始的w设置的已经接近的时候，梯度下降的很慢，因为已经很接近 局部最小值
     w = np.matrix(np.array([0]))
     # w = np.matrix(np.array([1]))
     b = 0
     # 体现了学习率的作用，学习率必须根据损失函数的偏导数调整，真实数据根据损失函数的偏导数很大,而w本身局部最小值较小时，容易发生梯度震荡
     w, b, cost = batch_gradient_descent(x_np, y_np, w.T, b, 0.000001, 1000)
-    tm.draw_2d_line(w.ravel()[0], b, x_range=(0, 100))
+    # tm.draw_2d_line(w.ravel()[0], b, x_range=(0, 100))
+    print(w)
+    print(b)
+    print(cost)
     return w, b, cost
+
+
+# train_by_sklearn 用sklearn训练
+def train_by_sklearn():
+    x_np, y_np = pd_data_to_np()
+    reg = lm.LinearRegression()
+    reg.fit(x_np, y_np)
+    print(reg.coef_)
+    print(reg.intercept_)
 
 
 # batch_gradient_descent 根据学习率和迭代次数进行批量梯度下降 θ[j]=θ[j]-α(əJ(θ)/əθ[j])
@@ -84,6 +104,7 @@ def batch_gradient_descent(X, y, w, b, learning_rate, num_iterations):
 # main 主函数
 def main():
     train()
+    train_by_sklearn()
 
 
 if __name__ == "__main__":
