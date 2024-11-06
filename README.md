d35ee271f13fc1c7245f6d71b8f849810271c976
王柏伟 <wangbw3@chinatelecom.cn>
2024-11-06 16:44:06 +0800

    #comment cycle-example

--- README.md
+++ README.md
@@ -37,3 +37,8 @@ Gitee 是 OSCHINA 推出的基于 Git 的代码托管平台（同时支持 SVN�
 4.  [GVP](https://gitee.com/gvp) 全称是 Gitee 最有价值开源项目，是综合评定出的优秀开源项目
 5.  Gitee 官方提供的使用手册 [https://gitee.com/help](https://gitee.com/help)
 6.  Gitee 封面人物是一档用来展示 Gitee 会员风采的栏目 [https://gitee.com/gitee-stars/](https://gitee.com/gitee-stars/)
+
+
+#### 题目
+1.  二手车价格评估回归 https://www.kaggle.com/competitions/playground-series-s4e9/overview
+2.  高脂肪风险多分类问题 https://www.kaggle.com/competitions/playground-series-s4e2
--- examples/secondhandcycle/cycle.py
+++ examples/secondhandcycle/cycle.py
@@ -1,3 +1,4 @@
+import re
 import string
 import datetime
 import numpy as np
@@ -20,15 +21,39 @@ def deal_data_with_avg_price(train_data, test_date: pd.DataFrame, name: string):
     print("成功转换字段:" + name)
 
 
+# device_column 将某列分裂为多列
+def device_column(data: pd.DataFrame):
+    d = data.loc[:, ['engine']]
+    data['mali'] = data['engine'].apply(lambda x: rep_str(x))
+    data['pailiang'] = data['engine'].apply(lambda x: rep_str(x, 1))
+    data['gang'] = data['engine'].apply(lambda x: rep_str(x, 2))
+    print(data.head())
+
+
+# rep_str
+def rep_str(s: string, index: int = 0):
+    ss = str(s).split(' ')
+    r_ss = [re.sub(r'[a-zA-Z/]', '', s_) for s_ in ss]
+    r_ss = r_ss + [None] * (3 - len(r_ss)) if len(r_ss) < 3 else r_ss
+    try:
+        f = float(r_ss[index])
+        return f
+    except Exception as e:
+        return None
+
+
 # read_data
 def deal_data():
     # 读取文件
     train_data = pd.read_csv(".\\train.csv")
     test_data = pd.read_csv(".\\test.csv")
+    device_column(train_data)
+    device_column(test_data)
     # 特征工程：品牌、型号年份、里程、发动机、变速器、额外颜色、初始颜色、事故历史、价格
     # brand,model_year,milage,engine,transmission,ext_col,int_col,accident,price
+    # 发动机-engine 需要分裂数据
     deal_data_with_avg_price(train_data, test_data, 'brand')
-    deal_data_with_avg_price(train_data, test_data, 'engine')
+    # deal_data_with_avg_price(train_data, test_data, 'engine')
     deal_data_with_avg_price(train_data, test_data, 'transmission')
     deal_data_with_avg_price(train_data, test_data, 'ext_col')
     deal_data_with_avg_price(train_data, test_data, 'int_col')
@@ -36,7 +61,7 @@ def deal_data():
     print(train_data.head())
     print(train_data.describe())
     train_data.to_csv('train_deal.csv', index=False)
-    train_data.to_csv('test_deal.csv', index=False)
+    test_data.to_csv('test_deal.csv', index=False)
 
 
 # read_data
@@ -44,12 +69,20 @@ def read_data():
     # 读取文件
     train_data = pd.read_csv(".\\train_deal.csv")
     test_data = pd.read_csv(".\\test_deal.csv")
+    # 特征提取
+    train_data = train_data.loc[:, ['brand', 'model_year', 'milage', 'mali', 'pailiang', 'gang', 'transmission', 'ext_col', 'int_col', 'accident', 'price']]
+    test_data = test_data.loc[:, ['brand', 'model_year', 'milage', 'mali', 'pailiang', 'gang', 'transmission', 'ext_col', 'int_col', 'accident', 'id']]
     # 空缺数据补空值--测试集和训练集都要
-    train_data.loc[test_data['accident'].isnull(), ['accident']] = 49024.80414354319
-    test_data.loc[test_data['accident'].isnull(), ['accident']] = 49024.80414354319
-    train_data = train_data.loc[:, ['brand', 'model_year', 'milage', 'engine', 'transmission', 'ext_col', 'int_col', 'accident', 'price']]
-    test_data = test_data.loc[:, ['brand', 'model_year', 'milage', 'engine', 'transmission', 'ext_col', 'int_col', 'accident', 'id']]
-    return train_data, test_data
+    col_mean = train_data.mean()
+    for col in train_data.columns:
+        train_data.fillna(col_mean[col], inplace=True)
+        test_data.fillna(col_mean[col], inplace=True)
+    # 特征缩放
+    train_data = train_data.div(train_data.max())
+    normalized_columns = [col for col in test_data.columns if col != 'id']
+    test_data[normalized_columns] = test_data[normalized_columns].div(train_data[normalized_columns].max())
+    # test_data = test_data.div(train_data.loc[:, ['brand', 'model_year', 'milage', 'mali', 'pailiang', 'gang', 'transmission', 'ext_col', 'int_col', 'accident']].max())
+    return train_data, test_data, train_data['price'].max()
 
 
 # pd_to_np pd格式转换为np格式
@@ -71,10 +104,9 @@ def read_init_model(file_name: string) -> keras.src.models.sequential.Sequential
         model = tf.keras.models.load_model(file_name)
     except Exception as e:
         model = tf.keras.models.Sequential([
-            # 特征缩放
-            # tf.keras.layers.experimental.preprocessing.Rescaling(1./5000),
-            # tf.keras.layers.experimental.preprocessing.Normalization(),
-            tf.keras.layers.Dense(16, activation='relu'),
+            # 此处加了一层32个神经元，分数提高了一点
+            tf.keras.layers.Dense(32),
+            tf.keras.layers.Dense(16),
             tf.keras.layers.Dense(1)
         ])
     finally:
@@ -90,31 +122,27 @@ def nn(train_d, train_l, test_d: pd.DataFrame):
     # 2.调整数据格式
     train_data = train_d.astype(np.float64)
     train_label = train_l.astype(np.float64)
-    train_data = train_data / 5000.
-    train_label = train_label / 300000.
-    print(train_data[0])
     # 4.1定义交叉熵损失函数
     loss_fn = tf.keras.losses.MeanSquaredError()
     model.compile(optimizer='adam', loss=loss_fn, metrics=['mse'])
     # 4.2定义tensorBoard
     tensorboard_callback = tb.draw_board('secondhandCycle')
     # 5.进行训练
-    train_history = model.fit(train_data, train_label, epochs=10, callbacks=[tensorboard_callback])
+    train_history = model.fit(train_data, train_label, epochs=100, callbacks=[tensorboard_callback])
     # 6.保存模型
     model.save('my_model.keras')
 
 
 # predict_test_data 预测测试集
-def predict_test_data(test_d, test_l: pd.DataFrame):
+def predict_test_data(test_d, test_l: pd.DataFrame, max_price: float):
     model = tf.keras.models.load_model('my_model.keras')
-    test_d = test_d.astype(np.float64)
-    test_d = test_d / 5000.
-    test_data = tf.reshape(tf.convert_to_tensor(test_d[:, 0:]), [len(test_d), 1, 6])
+    test_data = test_d
     pre = model.predict(test_data)
-    test_l = tf.reshape(tf.convert_to_tensor(test_l[:, 0:]), [len(test_l), 1])
-    pre = tf.reshape(tf.convert_to_tensor(pre[:, 0:]), [len(pre), 1])
-    pre = np.round(pre).astype(int)
-    result = np.concatenate((test_l, pre), axis=1)
+    pre = pre * max_price
+    # test_l = tf.reshape(tf.convert_to_tensor(test_l[:, 0:]), [len(test_l), 1])
+    # pre = tf.reshape(tf.convert_to_tensor(pre[:, 0:]), [len(pre), 1])
+    pre = np.round(pre).astype(float)
+    result = np.concatenate((test_l.astype(int), pre), axis=1)
     output = pd.DataFrame(result, columns=['id', 'price'])
     output.to_csv('submission.csv', index=False)
 
@@ -124,17 +152,20 @@ def predict_test_data(test_d, test_l: pd.DataFrame):
 
 # main 主函数
 def main():
+    # r = rep_str('177.0HP')
+    # print(r)
     # 1.数据特征处理
     # deal_data()
     # 1.pd读取数据集，提取相关有用信息并做数据预处理
-    train_data, test_data = read_data()
+    train_data, test_data, max_price = read_data()
     pd.set_option('display.max_rows', None)
     pd.set_option('display.max_columns', None)
     print(train_data.describe())
+    print(test_data.describe())
     # 2.整理字段
     train_d, train_l, test_d, test_l = pd_to_np(train_data, test_data)
-    nn(train_d, train_l, test_data)
-    # predict_test_data(test_d, test_l)
+    # nn(train_d, train_l, test_data)
+    predict_test_data(test_d, test_l, max_price)
 
 
 if __name__ == "__main__":
